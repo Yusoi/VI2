e49996418478839723b9bf1ced848bd033450c7e
Yusoi <bernardosilva.business@gmail.com>
2020-03-22 19:58:12 +0000

    Fixed

--- TP1/optix/raytracing.cu
+++ TP1/optix/raytracing.cu
@@ -19,6 +19,7 @@ enum { PHONG_RAY_TYPE = 0, SHADOW_RAY_TYPE,  RAY_TYPE_COUNT };
 
 //closest hit radiance
 extern "C" __global__ void __closesthit__radiance() {
+    
     float3 &prd = *(float3*)getPRD<float3>();
 
     const TriangleMeshSBTData &sbtData = *(const TriangleMeshSBTData*)optixGetSbtDataPointer();
@@ -45,12 +46,11 @@ extern "C" __global__ void __closesthit__radiance() {
     // intersection position
     const float3 surfPos = optixGetWorldRayOrigin() + optixGetRayTmax()*optixGetWorldRayDirection();
 
-    // Face forward + Normalizatio
-    const float3 rayDir = optixGetWorldRayDirection();
-    const float3 lightDir = make_float3(optixLaunchParams.global->lightDir);
+    // Face forward + Normalization
+    const float3 lightDir = -make_float3(optixLaunchParams.global->lightDir);
     float3 Ns = normalize(n);
 
-    float intensity = max(dot(lightDir, Ns),0.0f);
+    float intensity = max(dot(lightDir, Ns),0.5f);
 
     // Set payload
     float lightVisibility = 1.0f;
@@ -62,10 +62,11 @@ extern "C" __global__ void __closesthit__radiance() {
                surfPos,
                lightDir,
                0.001f,      // tmin
-               0.5f,  // tmax
+               1e20f,  // tmax
                0.0f,       // rayTime
                OptixVisibilityMask( 255 ),
-               OPTIX_RAY_FLAG_NONE,
+               OPTIX_RAY_FLAG_DISABLE_ANYHIT
+               | OPTIX_RAY_FLAG_TERMINATE_ON_FIRST_HIT,
                SHADOW_RAY_TYPE,            // SBT offset
                RAY_TYPE_COUNT,               // SBT stride
                SHADOW_RAY_TYPE,            // missSBTIndex 
@@ -75,12 +76,12 @@ extern "C" __global__ void __closesthit__radiance() {
     if (sbtData.hasTexture && sbtData.vertexD.texCoord0) {
       const float4 tc = (1.f-u-v) * sbtData.vertexD.texCoord0[index.x] + u * sbtData.vertexD.texCoord0[index.y] + v * sbtData.vertexD.texCoord0[index.z];
       float4 fromTexture = tex2D<float4>(sbtData.texture,tc.x,tc.y);
-      prd = make_float3(fromTexture) * min(intensity * lightVisibility + 0.0, 1.0);
+      prd = make_float3(fromTexture) * min(intensity * lightVisibility, 1.0);
     }
     
     else{
         // Final shading: ambient, directional ambient and shadowing
-        prd = sbtData.color * min(intensity * lightVisibility + 0.0, 1.0);
+        prd = sbtData.color * min(intensity * lightVisibility, 1.0);
     }
 }
 
@@ -93,13 +94,13 @@ extern "C" __global__ void __anyhit__radiance() {
 extern "C" __global__ void __miss__radiance() {
     float3 &prd = *(float3*)getPRD<float3>();
     // white background color
-    prd = make_float3(1.0f, 1.0f, 1.0f);
+    prd = make_float3(0.0f, 0.8f, 0.0f);    
 }
 
 //closest hit shadow
 extern "C" __global__ void __closesthit__shadow() {
     float &prd = *(float*)getPRD<float>();
-    prd = 0.0f;
+    prd = 0.5f;
 }
 
 //any hit shadow
@@ -112,36 +113,6 @@ extern "C" __global__ void __miss__shadow() {
     // we didn't hit anything, so the light is visible
     float &prd = *(float*)getPRD<float>();
     prd = 1.0f;
-}
-
-//-----------------
-// Luzes
-extern "C" __global__ void __closesthit__light() {
-
-    float3 &prd = *(float3*)getPRD<float3>();
-    prd = make_float3(1.0f, 1.0f, 1.0f);
-}
-
-
-extern "C" __global__ void __anyhit__light() {
-}
-
-
-extern "C" __global__ void __miss__light() {
-}
-
-
-extern "C" __global__ void __closesthit__light_shadow() {
-    float &prd = *(float*)getPRD<float>();
-    prd = 1.0f;
-}
-
-// Ignore intersections
-extern "C" __global__ void __anyhit__light_shadow() {
-}
-
-// miss empty for background
-extern "C" __global__ void __miss__light_shadow() {
 }
 
 //----------
@@ -162,7 +133,8 @@ extern "C" __global__ void __miss__radiance_grade() {
 
 //closest hit shadow para grades
 extern "C" __global__ void __closesthit__shadow_grade() {
-
+    float &prd = *(float*)getPRD<float>();
+    prd = 0.5f;
 }
 
 //any hit shadow para grades
@@ -172,8 +144,17 @@ extern "C" __global__ void __anyhit__shadow_grade() {
 
 //miss shadow para grades
 extern "C" __global__ void __miss__shadow_grade() {
-
+    //TODO
+    // we didn't hit anything, so the light is visible
+    float &prd = *(float*)getPRD<float>();
+    prd = 1.0f;
 }
+
+/**
+*
+*   VIDRO
+*
+*/
 
 //closest hit radiance para vidros
 extern "C" __global__ void __closesthit__radiance_vidro() {
@@ -187,12 +168,13 @@ extern "C" __global__ void __anyhit__radiance_vidro() {
 
 //miss radiance para vidros
 extern "C" __global__ void __miss__radiance_vidro() {
-    
+
 }
 
 //closest hit shadow para vidros
 extern "C" __global__ void __closesthit__shadow_vidro() {
-
+    float &prd = *(float*)getPRD<float>();
+    prd = 0.5f;
 }
 
 //any hit shadow para vidros
@@ -202,7 +184,8 @@ extern "C" __global__ void __anyhit__shadow_vidro() {
 
 //miss shadow para vidros
 extern "C" __global__ void __miss__shadow_vidro() {
-
+    float &prd = *(float*)getPRD<float>();
+    prd = 1.0f;
 }
 
 //Ray Deployment
@@ -231,55 +214,35 @@ extern "C" __global__ void __raygen__renderFrame() {
         float3 pixelColorPRD = make_float3(1.f);
         uint32_t u0, u1;
         packPointer( &pixelColorPRD, u0, u1 );  
-    
-        float raysPerPixel = float(optixLaunchParams.frame.raysPerPixel);
-        // half pixel
-        float2 delta = make_float2(1.0f/raysPerPixel, 1.0f/raysPerPixel);
-    
-        // compute ray direction
-        // normalized screen plane position, in [-1, 1]^2
-      
         float red = 0.0f, blue = 0.0f, green = 0.0f;
-        for (int i = 0; i < raysPerPixel; ++i) {
-            for (int j = 0; j < raysPerPixel; ++j) {
-                float2 subpixel_jitter;
-                uint32_t seed = tea<4>( ix * optixGetLaunchDimensions().x + iy, i*raysPerPixel + j );
-                if (optixLaunchParams.global->jitterMode == 0)
-                    subpixel_jitter = make_float2(i * delta.x, j * delta.y);
-                else if (optixLaunchParams.global->jitterMode == 1)
-                    subpixel_jitter = make_float2( rnd( seed )-0.5f, rnd( seed )-0.5f );
-                else 
-                    subpixel_jitter = make_float2( i * delta.x + delta.x *  rnd( seed ), j * delta.y + delta.y * rnd( seed ) );
-                
-                
-                const float2 screen(make_float2(ix + subpixel_jitter.x, iy + subpixel_jitter.y)
-                                / make_float2(optixGetLaunchDimensions().x, optixGetLaunchDimensions().y) * 2.0 - 1.0);
+        
+
+        const float2 screen(make_float2(ix+.5f,iy+.5f)
+                        / make_float2(optixGetLaunchDimensions().x, optixGetLaunchDimensions().y) * 2.0 - 1.0);
+
+        // note: nau already takes into account the field of view and ratio when computing 
+        // camera horizontal and vertival
+        float3 rayDir = normalize(camera.direction
+                                + (screen.x ) * camera.horizontal
+                                + (screen.y ) * camera.vertical);
             
-            // note: nau already takes into account the field of view and ratio when computing 
-            // camera horizontal and vertival
-                float3 rayDir = normalize(camera.direction
-                                    + (screen.x ) * camera.horizontal
-                                    + (screen.y ) * camera.vertical);
-                
-                // trace primary ray
-                optixTrace(optixLaunchParams.traversable,
-                        camera.position,
-                        rayDir,
-                        0.f,    // tmin
-                        1e20f,  // tmax
-                        0.0f,   // rayTime
-                        OptixVisibilityMask( 255 ),
-                        OPTIX_RAY_FLAG_NONE,//,OPTIX_RAY_FLAG_DISABLE_ANYHIT
-                        PHONG_RAY_TYPE,             // SBT offset
-                        RAY_TYPE_COUNT,               // SBT stride
-                        PHONG_RAY_TYPE,             // missSBTIndex 
-                        u0, u1 );
-    
-                red += pixelColorPRD.x / (raysPerPixel*raysPerPixel);
-                green += pixelColorPRD.y / (raysPerPixel*raysPerPixel);
-                blue += pixelColorPRD.z / (raysPerPixel*raysPerPixel);
-            }
-    }
+        // trace primary ray
+        optixTrace(optixLaunchParams.traversable,
+                camera.position,
+                rayDir,
+                0.f,    // tmin
+                1e20f,  // tmax
+                0.0f,   // rayTime
+                OptixVisibilityMask( 255 ),
+                OPTIX_RAY_FLAG_DISABLE_ANYHIT,//,OPTIX_RAY_FLAG_DISABLE_ANYHIT
+                PHONG_RAY_TYPE,             // SBT offset
+                RAY_TYPE_COUNT,               // SBT stride
+                PHONG_RAY_TYPE,             // missSBTIndex 
+                u0, u1 );
+
+        red += pixelColorPRD.x;
+        green += pixelColorPRD.y;
+        blue += pixelColorPRD.z;
     
         //convert float (0-1) to int (0-255)
         const int r = int(255.0f*red);
